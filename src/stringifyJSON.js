7e80b31bad0a643a9e55d9e7b3dda808c7894e27
Brandon Borders <brandon@camelcased.com>
2015-07-14 13:59:51 -0700

    Implement `stringifyJSON(obj)`

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,35 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var json = [];
+  if (obj === undefined || typeof(obj) === 'function') {
+    return;
+  } else if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null || typeof(obj) !== 'object'){
+    return String(obj);
+  } else if (Array.isArray(obj)) {
+    for (var i = 0;i < obj.length;i++) {
+      json.push(stringifyJSON(obj[i]));
+    }
+
+    return '[' + json.join(',') + ']';
+  } else {
+    var keys = Object.keys(obj);
+    for (var key in obj) {
+      var stringified = stringifyJSON(obj[key]);
+
+      if (stringified !== undefined) {
+        json.push('"' + key + '":');
+        json.push(stringified);
+
+        if (keys.length > 1 && keys.indexOf(key) !== keys.length - 1) {
+          json.push(',');
+        }
+      }
+    }
+
+    return '{' + json.join('') + '}';
+  }
 };
